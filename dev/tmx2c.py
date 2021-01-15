c64b53ddcc9463ba05f72d1030b1d22224095ed2
Sebastian Riedel <git@basxto.de>
2021-01-15 06:44:52 +0100

    Fix metatile indices during generation

--- dev/tmx2c.py
+++ dev/tmx2c.py
@@ -4,6 +4,13 @@ import argparse
 import os
 import sys
 
+#decrement integer in string
+def decstr(s):
+    if s == '' or s == 0:
+        return s
+    else:
+        return str(int(s)-1)
+
 parser = argparse.ArgumentParser()
 parser.add_argument('tmx', metavar='map.tmx', help='Tiled map file')
 parser.add_argument("--output", "-o", default="", help="Name for generated c files")
@@ -31,6 +38,9 @@ file.write('//Generated from ' + filename + '\n')
 layers = xmldoc.getElementsByTagName('layer')
 for l in layers:
     name = os.path.basename(filename).split('.')[0] + '_tmap_' + l.attributes['name'].value
-    file.write('const uint8_t ' + name + '[] = {' + l.getElementsByTagName('data')[0].firstChild.nodeValue + '};\n')
+    row = l.getElementsByTagName('data')[0].firstChild.nodeValue
+    # decrease each number 1,2,3->0,1,2
+    row = ",".join(map(lambda s: decstr(s),row.split(',')))
+    file.write('const uint8_t ' + name + '[] = {' + row + '};\n')
 file.close()
 print('File written to ' + cfilename)
